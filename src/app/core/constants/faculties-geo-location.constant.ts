9e1eb733ed09c1ad1729e365bd83d475b24e2ada
Margareta Galaju <m.galaju@emendis.nl>
2022-06-08 16:44:41 +0300

    Connect multiple panoramas between themselves

--- src/app/core/constants/faculties-geo-location.constant.ts
+++ src/app/core/constants/faculties-geo-location.constant.ts
@@ -5,58 +5,110 @@ export const fcim = {
   },
   additionalPanoramas: [
     {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim", // The ID for this custom this.panorama.
-      description: "Entrance of Faculty of Technical University of Moldova",
+      imagePath: '/assets/panoramas/fcim/first.jpg',
+      pano: 'first', // The ID for this custom this.panorama.
+      description: 'First',
+      position: {
+        lat:  47.061621,
+        long: 28.867827,
+      },
+      links: [
+        {
+          heading: 25,
+          description: 'FCIM first',
+          pano: 'fcim',
+        }
+      ],
+    },
+    {
+      imagePath: '/assets/panoramas/fcim/entrance.JPG',
+      pano: 'fcim', // The ID for this custom this.panorama.
+      description: 'Entrance of Faculty of Technical University of Moldova',
       position: {
         lat: 47.061606,
         long: 28.867924,
-      }
+      },
+      links: [
+        {
+          heading: 25,
+          description: 'FCIM first corridor',
+          pano: 'fcim_corridor1',
+        },
+        {
+          heading: 195,
+          description: 'back to entrance',
+          pano: 'first',
+        },
+      ],
     },
     {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim_corridor1", // The ID for this custom this.panorama.
-      description: "Corridor 1 of Faculty of Technical University of Moldova",
+      imagePath: '/assets/panoramas/fcim/entrance.JPG',
+      pano: 'fcim_corridor1', // The ID for this custom this.panorama.
+      description: 'Corridor 1 of Faculty of Technical University of Moldova',
       position: {
         lat: 47.061918,
         long: 28.868000,
-      }
+      },
+      links: [
+        {
+          heading: 25,
+          description: 'FCIM second corridor',
+          pano: 'fcim_corridor2',
+        },
+        {
+          heading: 195,
+          description: 'back to entrance',
+          pano: 'fcim',
+        },
+      ],
     },
     {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim_corridor2", // The ID for this custom this.panorama.
-      description: "Corridor 2 of Faculty of Technical University of Moldova",
+      imagePath: '/assets/panoramas/fcim/entrance.JPG',
+      pano: 'fcim_corridor2', // The ID for this custom this.panorama.
+      description: 'Corridor 2 of Faculty of Technical University of Moldova',
       position: {
         lat: 47.062118,
         long: 28.868110,
-      }
+      },
+      links: [
+        {
+          heading: 25,
+          description: 'FCIM third corridor',
+          pano: 'fcim_corridor3',
+        },
+        {
+          heading: 195,
+          description: 'back to first corridor',
+          pano: 'fcim_corridor1',
+        },
+      ],
     },
-    {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim_corridor3", // The ID for this custom this.panorama.
-      description: "Corridor 3 of Faculty of Technical University of Moldova",
-      position: {
-        lat: 47.062578,
-        long: 28.868310,
-      }
-    },
-    {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim_class1", // The ID for this custom this.panorama.
-      description: "Classrom 1 of Faculty of Technical University of Moldova",
-      position: {
-        lat: 47.062588,
-        long: 28.868270,
-      }
-    },
-    {
-      imagePath: "/assets/panoramas/fcim/entrance.JPG",
-      pano: "fcim_class2", // The ID for this custom this.panorama.
-      description: "Classrom 2 of Faculty of Technical University of Moldova",
-      position: {
-        lat: 47.062588,
-        long: 28.868390,
-      }
-    },
+    // {
+    //   imagePath: '/assets/panoramas/fcim/entrance.JPG',
+    //   pano: 'fcim_corridor3', // The ID for this custom this.panorama.
+    //   description: 'Corridor 3 of Faculty of Technical University of Moldova',
+    //   position: {
+    //     lat: 47.062578,
+    //     long: 28.868310,
+    //   }
+    // },
+    // {
+    //   imagePath: '/assets/panoramas/fcim/entrance.JPG',
+    //   pano: 'fcim_class1', // The ID for this custom this.panorama.
+    //   description: 'Classrom 1 of Faculty of Technical University of Moldova',
+    //   position: {
+    //     lat: 47.062588,
+    //     long: 28.868270,
+    //   }
+    // },
+    // {
+    //   imagePath: '/assets/panoramas/fcim/entrance.JPG',
+    //   pano: 'fcim_class2', // The ID for this custom this.panorama.
+    //   description: 'Classrom 2 of Faculty of Technical University of Moldova',
+    //   position: {
+    //     lat: 47.062588,
+    //     long: 28.868390,
+    //   }
+    // },
   ]
 }
--- src/app/features/home/components/virtual-tour/virtual-tour.component.ts
+++ src/app/features/home/components/virtual-tour/virtual-tour.component.ts
@@ -1,8 +1,7 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { fcim } from 'src/app/core/constants/faculties-geo-location.constant';
 import { EngineService } from 'src/app/core/services/3d-map/engine.service';
-import { FacultyApiService } from 'src/app/core/services/faculty-api.service';
 import { Faculty } from 'src/app/shared/models/faculty.model';
 
 declare var google: any;
@@ -18,7 +17,7 @@ declare global {
   selector: 'app-virtual-tour',
   templateUrl: './virtual-tour.component.html',
   styleUrls: ['./virtual-tour.component.scss'],
-  providers: [EngineService]
+  providers: [EngineService],
 })
 export class VirtualTourComponent {
   @ViewChild('rendererCanvas', {static: true})
@@ -28,81 +27,58 @@ export class VirtualTourComponent {
   
   public panorama: google.maps.StreetViewPanorama;
 
-  public outsideGoogle: google.maps.StreetViewPanoramaData;
-  
   constructor(
     private router: Router,
   ) {
-    window.initMap = new google.maps.StreetViewService()
-      .getPanorama({ location: { lat: 47.061621, lng: 28.867827 }}, (data) => {
-        this.outsideGoogle = data;
-        this.initPanorama();
-      });
+    new google.maps.StreetViewService()
+    .getPanorama({ location: { lat: 47.061621, lng: 28.867827 }}, (data) => {
+      this.initPanorama();
+    });
    }
 
-  public getFacultyPanoramaData(panorama): google.maps.StreetViewPanoramaData {
+  public getAdditionalPanorama(panorama): google.maps.StreetViewPanoramaData {
     return {
       location: {
         pano: panorama.pano,
         description: panorama.description,
         latLng: new google.maps.LatLng(panorama.position.lat, panorama.position.long),
-    },
-      links: [
-        {
-          heading: 195,
-          description: "Exit",
-          pano: (this.outsideGoogle.location as google.maps.StreetViewLocation).pano,
-        },
-        // {
-        //   heading: 25,
-        //   description: "Go further",
-        //   pano: (this.outsideGoogle.location as google.maps.StreetViewLocation).pano,
-        // },
-      ],
-      copyright: "Unipply (c)",
+      },
+      links: panorama.links,
+      copyright: 'Unipply (c)',
       tiles: {
         tileSize: new google.maps.Size(6912, 3456),
         worldSize: new google.maps.Size(6912, 3456),
         centerHeading: 105,
-        getTileUrl: function (
-          pano: string,
-          zoom: number,
-          tileX: number,
-          tileY: number
-        ): string {
-          console.log('here');
-          
-          return panorama.imagePath
-        },
+        getTileUrl: () => panorama.imagePath,
       },
     };
   }
   
-  public initPanorama() {
+  public initPanorama(): void {
     this.panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("street-view") as HTMLElement,
-      { pano: (this.outsideGoogle.location as google.maps.StreetViewLocation).pano }
+      document.getElementById('street-view') as HTMLElement,
+      { pano: 'first', visible: true }
     );
 
-    this.panorama.registerPanoProvider(
-      (pano: string): google.maps.StreetViewPanoramaData => {
-        if (pano === "fcim") {
-          return this.getFacultyPanoramaData(fcim.additionalPanoramas[0]);
-        }
-        return null;
-      }
-    );
+    this.panorama.registerPanoProvider(() => {
+      return this.getAdditionalPanorama(fcim.additionalPanoramas[0]);
+    });
   
-    this.panorama.addListener("links_changed", () => {
-      if (
-        this.panorama.getPano() ===
-        (this.outsideGoogle.location as google.maps.StreetViewLocation).pano
-      ) {
-        this.panorama.getLinks().push({
-          description: "FCIM",
-          heading: 25,
-          pano: "fcim",
+    this.panorama.addListener('pano_changed', () => {
+      const newPano = fcim.additionalPanoramas.find((p) => p.pano === this.panorama.getPano())
+      
+      if (newPano) {
+        this.panorama.registerPanoProvider(() => {
+          return this.getAdditionalPanorama(newPano);
         });
+      }
+    })
+
+    this.panorama.addListener('links_changed', () => {
+      const currentPano = fcim.additionalPanoramas.find((p) => p.pano === this.panorama.getPano())
+
+      if (!!currentPano) {
+        this.panorama.getLinks().push(...currentPano.links);
       }
     });
   }
