0295015ef7380b0976935c518bd792d62c806e68
Margareta Galaju <m.galaju@emendis.nl>
2022-06-11 15:36:37 +0300

    Bug fixes

--- src/app/core/constants/faculty-id.enum.ts
+++ src/app/core/constants/faculty-id.enum.ts
@@ -5,7 +5,7 @@ export enum FacultyIdEnum {
   FMIET  = '78050741-da48-451d-bdc7-e05031aa724e',
   FEEB = '5ec9f43e-b68c-48cc-a983-1463716f8b03',
   FFT = '41ba4ac4-5991-4977-a134-7c7750095021',
-  FET = ' 13f040e8-f953-4eb4-af06-a56501e10ef0',
+  FET = '13f040e8-f953-4eb4-af06-a56501e10ef0',
   FCGC = '0f64242d-fda3-4885-8a7f-4624731adfde',
   FTP = '0e2aed9e-99eb-41c4-a3ad-a06bf63459e2',
 }
